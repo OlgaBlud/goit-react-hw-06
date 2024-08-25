540737c3c3e10ac058ee325e25a2ab4970614dd9
Olga Blud <olga.blud@gmail.com>
2024-08-25 17:05:32 +0200

    add Local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,36 @@
 // import { filtersReducer } from "./filtersSlice";
 
 import { configureStore } from "@reduxjs/toolkit";
+// persistStorage
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
-
+const contactsConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["items"], // blacklist: ["showProfilesList"]
+};
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(contactsConfig, contactsReducer),
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
